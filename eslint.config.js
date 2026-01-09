8c0c0ad177c7f3724b448b838d0d7f1845bc73b5
Roman Berdnikov <berdnikov.roma@gmail.com>
2026-01-09 10:10:39 +0300

    добавил хаски

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
+    plugins: {
+      import: importPlugin,
+    },
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          pathGroups: [
+            { pattern: 'react{,/**}', group: 'external', position: 'before' },
+
+            { pattern: '@/app/**', group: 'internal', position: 'before' },
+            { pattern: '@/pages/**', group: 'internal', position: 'before' },
+
+            { pattern: '@/widgets/**', group: 'internal', position: 'after' },
+            { pattern: '@/features/**', group: 'internal', position: 'after' },
+            { pattern: '@/entities/**', group: 'internal', position: 'after' },
+            { pattern: '@/shared/**', group: 'internal', position: 'after' },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
   },
-])
+]);
--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '~/app/App';
+
 import { TodoPageLazy } from './lazyLoaders';
+
+import App from '~/app/App';
 import Home from '~/pages/home';
 import { ROUTES } from '~/shared/config';
 
