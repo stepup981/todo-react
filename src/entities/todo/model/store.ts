5d272c9effe6d8d4e39a518fe44ce4ae270c3f6a
Roman Berdnikov <berdnikov.roma@gmail.com>
2026-01-13 09:39:41 +0300

    нейминг интерфейса стора

--- src/entities/todo/model/store.ts
+++ src/entities/todo/model/store.ts
@@ -2,12 +2,12 @@ import { create } from 'zustand';
 
 import type { Todo } from './types';
 
-interface IStoreTodo {
+interface StoreTodo {
   todos: Todo[];
   setTodos: (todos: Todo[]) => void;
 }
 
-export const storeTodo = create<IStoreTodo>(set => ({
+export const storeTodo = create<StoreTodo>(set => ({
   todos: [],
   setTodos: todos => set({ todos }),
 }));
