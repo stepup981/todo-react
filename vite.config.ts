de2c115f56802287747911f0bd16a43d1298ddbc
Roman Berdnikov <berdnikov.roma@gmail.com>
2026-02-26 10:20:46 +0300

    подкинул виз.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const atAlias = path.resolve(__dirname, './src');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './bundle-visualizer.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: { '~': atAlias },
   },
